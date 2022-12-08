3c9dd9ab2a20027c74cd96fb37960c3a47002500
zzz <zzz@zzz.com>
2022-12-08 15:45:43 +0800

    add i18n example

---
+++ vue3-i18n-example/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="$t('welcomeMessage')" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
